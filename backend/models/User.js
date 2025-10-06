504ccda673ac18abd7966d7cdd20d27273990c6d
ghasnhhz <shahzodkarimov1001@gmail.com>
2025-10-06 21:16:07 +0500

    Implement authentication: register, login, refresh, and logout

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -22,5 +23,19 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   }
 })
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  try {
+    this.password = await bcrypt.hash(this.password, 10)
+    return next()
+  } catch (err) {
+    next(err)
+  }
+})
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password)
+}
 
 module.exports = mongoose.model("User", userSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 require("dotenv").config({quiet: true})
 const express = require("express")
+const cookieParser = require("cookie-parser")
+const authRoutes = require("./routes/auth")
+const errorHandler = require("./middlewares/errorHandler")
 const connectDB = require("./db/connect")
 const app = express()
+
+connectToDB()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/auth", authRoutes)
+
+app.use(errorHandler)
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is listening on: http://localhost/${process.env.PORT}`)
+})
 
 async function connectToDB() {
   try {
@@ -11,9 +26,3 @@ async function connectToDB() {
     console.log("Failed to connect to the database", err.message)
   }
 }
-connectToDB()
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`App is listening on: http://localhost/${process.env.PORT}`)
-})
